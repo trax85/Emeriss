f424bff19029514714cf205c7e14578472425b2d
mortezadadgar <mortezadadgar97@gmail.com>
2019-09-18 21:47:34 +0430

    Revert missing update_rq_clock commits
    we'll re-check later.
    
    Revert "UPSTREAM: sched/core: Add missing update_rq_clock() call for task_hot()"
    
    This reverts commit e313546c0cb0b478ff5a233c93f9344bb32c0fda.
    
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>
    
    Revert "UPSTREAM: sched/core: Add missing update_rq_clock() call in set_user_nice()"
    
    This reverts commit ce0ef05751f1c2e73c7c131512414bbf7308fa19.
    
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>
    
    Revert "sched/fair: Update rq clock before changing a task's CPU affinity"
    
    This reverts commit 86896dd9468db8094dc4752f6253b71c28b01782.
    
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8521,7 +8521,6 @@ redo:
 
 more_balance:
 		raw_spin_lock_irqsave(&busiest->lock, flags);
-		update_rq_clock(busiest);
 
 		/*
 		 * cur_ld_moved - load moved in current iteration
@@ -8922,7 +8921,6 @@ static int active_load_balance_cpu_stop(void *data)
 		};
 
 		schedstat_inc(sd, alb_count);
-		update_rq_clock(busiest_rq);
 
 		p = detach_one_task(&env);
 		if (p) {
