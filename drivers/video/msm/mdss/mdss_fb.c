bcca3734d64c1fe94bb70f2d03d60d5ec8680b46
Pierre2324 <pdbbasketball@gmail.com>
2020-03-08 22:54:28 -0400

    msm: mdss: Boost DDR bus in critical path of rendering display frames
    Try to reduce jank by requesting a boost.
    Previous way wasn't the best way to do it and leaded to increased temps.
    This is done on newer kernels on committing new frames.
    Let's try it this way on 3.10.x.
    
    Signed-off-by: Pierre2324 <pdbbasketball@gmail.com>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -54,11 +54,11 @@
 #ifdef CONFIG_MACH_XIAOMI_KENZO
 #include "mdss_dsi.h"
 #endif
+#include <linux/devfreq_boost.h>
 #include "mdss_fb.h"
 #include "mdss_mdp_splash_logo.h"
 #define CREATE_TRACE_POINTS
 #include "mdss_debug.h"
-
 #include "mdss_livedisplay.h"
 
 #ifdef CONFIG_FB_MSM_TRIPLE_BUFFER
@@ -3919,6 +3919,7 @@ int mdss_fb_do_ioctl(struct fb_info *info, unsigned int cmd,
 		break;
 
 	case MSMFB_DISPLAY_COMMIT:
+		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
 		ret = mdss_fb_display_commit(info, argp);
 		break;
 
